acb4b70398851bfe7948704d975b512f1f0c4b20
yamanoyuuma <yamano.rock.1202@gmail.com>
2020-08-31 14:05:57 +0900

    2-2

---
+++ 2-2/index.php
@@ -0,0 +1,28 @@
+<?php
+$message = "Hello World!";
+var_dump($message);
+
+$x = 8;
+$y = 5.55;
+echo '<br>';
+var_dump($x);
+echo '<br>';
+var_dump($y);
+echo '<br>';
+$flag = true;
+var_dump($flag);
+echo '<br>';
+$emp = null;
+var_dump($emp);
+echo '<br>';
+$en_words = 'Hello World';
+var_dump($en_words);
+echo '<br>';
+$ja_words = 'こんにちは';
+var_dump($ja_words);
+echo '<br>';
+echo "1.API"."<br>";
+echo "プログラムや機能に対するお仕事依頼用の窓口"."<br>";
+echo "2.SVN（Subversion）"."<br>";
+echo "バージョン管理システムのひとつでありファイルの変更履歴を管理してくれるシステムのひとつ 複数の開発者が共同で一つのソフトウェアを開発する際などに、ソースコードやドキュメントを管理するのに用いられる。"
+?>
