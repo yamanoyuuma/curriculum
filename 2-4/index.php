9d4a0e0f0e354dd5080a7604bebf7e5384d64958
yamanoyuuma <yamano.rock.1202@gmail.com>
2020-08-31 15:11:42 +0900

    2-4

---
+++ 2-4/index.php
@@ -0,0 +1,64 @@
+<?php
+echo 1 + 2;
+echo '<br>';
+echo 2 - 1;
+echo '<br>';
+echo 2 * 2;
+echo '<br>';
+echo 4 / 2;
+echo '<br>';
+echo 5 % 2;
+echo '<br>';
+// 単項演算子
+// $x = $x + 1
+$x++; 
+echo $x;
+echo '<br>';
+// $x = $x - 1
+$x--;
+$x++; 
+echo $x;
+echo '<br>';
+$i = 1;
+$i++;
+echo $i;
+echo '<br>';
+$l = 5;
+$l--;
+echo $l;
+echo '<br>';
+$x = 0;
+ // $x = $x + 5;
+$x += 5;
+echo $x;
+echo '<br>';
+ // $x = $x * 5;
+$x *= 5;
+echo $x;
+echo '<br>';
+
+$y = 10;
+
+// $y = $y - 5;
+$y -= 5;
+echo $y;
+
+echo '<br>';
+
+// $y = $y * 5;
+$y *=5;
+echo $y;
+echo '<br>';
+
+echo "1.エビデンス"."<br>";
+echo "証拠のこと テストに使ったデータ
+・ プログラムを動かした結果の画面（ハードコピー）
+・プログラムを動かしたときの記録（ログ）"."<br>";
+
+echo "2.バッチ"."<br>";
+echo "「1回分にまとめる」という意味 どこかのタイミングでまとめてやる処理のこと"."<br>";
+
+echo "3.MVCモデル"."<br>";
+echo "プログラムの中身を「モデル（Model）」「ビュー（View）」「コントローラ（Controller）」の3つに分類しておくこと"."<br>";
+?>
+
