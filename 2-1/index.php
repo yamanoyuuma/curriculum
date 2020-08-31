6433aeae36d0483edd4cf9f4330a503f4540d4ec
yamanoyuuma <yamano.rock.1202@gmail.com>
2020-08-31 13:37:24 +0900

    2-1

---
+++ 2-1/index.php
@@ -0,0 +1,28 @@
+<?php
+// PHPの研修カリキュラム2-1
+?>
+<?php
+/*
+PHPの
+研修
+カリキュラム
+2-1
+*/
+?>
+<html>
+    <p>I say that <?php echo "Hello World!!"; ?></p>
+</html>
+
+<?php
+echo "1.開発環境・検証環境・本番環境"."<br>";
+echo "開発環境・開発するときの作業場所
+    検証環境・開発し終わった、リリース予定の機能をテストするための環境
+    本番環境・サービス提供の、実運用を行う環境"."<br>";
+echo "2.基本設計書・詳細設計書"."<br>";
+echo "基本設計書・お客さまに見えるところ」を考える作業。
+    システム全体の概要や、大雑把な機能の一覧、どんな機器で動かすかや、どんなスケジュールで作業を進めるかなどを考える。
+    詳細設計書・「お客様に見えないところ」を考える作業。
+    プログラムの構造やデータの流れなどの細かい部分まで考える。"."<br>";
+echo "3.Ajax"."<br>";
+echo "JavaScriptを使って非同期通信をする"."<br>";
+?><img class="alignnone size-full wp-image-371" src="http://54.238.147.46/wp-content/uploads/2020/04/update_task_status.png" alt="" width="716" height="180" data-pagespeed-url-hash="608921261" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
