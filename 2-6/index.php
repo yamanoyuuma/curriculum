fab92045ed8b94157e1d79578d5ab23b77d599d9
yamanoyuuma <yamano.rock.1202@gmail.com>
2020-08-31 17:06:08 +0900

    2-6

---
+++ 2-6/index.php
@@ -0,0 +1,15 @@
+<?php
+for($num = 0;$num < 101;$num++) {
+    echo $num;
+    echo '<br>';
+}
+
+echo "1.サブクエリ"."<br>";
+echo "SQL文を入れ子にする（SQL文の結果を使ったSQL文にする）こと、もしくは、SQL文を入れ子にしたSQL文における中に書いてある方のSQL文のこと"."<br>";
+
+echo "2.Bootstrap"."<br>";
+echo "HTML／CSS／JavaScriptから構成される最も有名なWEBフレームワーク"."<br>";
+
+echo "3.ライブラリ・フレームワーク"."<br>";
+echo "何かの枠組みのこと。システム開発を楽に行えるように用意された、プログラムとかのひな形のこと"
+?>
